03c38a87a577ca14b7182840d7d3fc3b7f1e36c3
Nachomen3322 <nachomen3322@gmail..com>
2023-12-13 01:36:49 -0400

    septima

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -9,34 +9,54 @@ export const Navbar = () => {
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <NavLink to="/" className="nav-link active">Inicio</NavLink>
+          <NavLink to="/" className="nav-link active">
+            Inicio
+          </NavLink>
         </li>
         <li className="nav-item">
-          <NavLink to="/about" className="nav-link active">Datos</NavLink>
+          <NavLink to="/about" className="nav-link active">
+            Datos
+          </NavLink>
         </li>
         <li className="nav-item">
-          <NavLink to="/teachers" className="nav-link active">teachers</NavLink>
+          <NavLink to="/teachers" className="nav-link active">
+            teachers
+          </NavLink>
         </li>
         <li className="nav-item">
-          <NavLink to="/courses" className="nav-link active">courses</NavLink>
+          <NavLink to="/courses" className="nav-link active">
+            courses
+          </NavLink>
         </li>
         <li className="nav-item">
-          <NavLink to="/events" className="nav-link active">events</NavLink>
+          <NavLink to="/events" className="nav-link active">
+            events
+          </NavLink>
         </li>
         <li className="nav-item">
-          <NavLink to="/sign-in" className="nav-link active">Sign in</NavLink>
+          <NavLink to="/sign-in" className="nav-link active">
+            Sign in
+          </NavLink>
         </li>
         <li className="nav-item">
-          <NavLink to="/testimonios" className="nav-link active">Testimonios</NavLink>
+          <NavLink to="/testimonios" className="nav-link active">
+            Testimonios
+          </NavLink>
         </li>
         <li className="nav-item">
-          <NavLink to="/blog" className="nav-link active">Blog</NavLink>
+          <NavLink to="/blog" className="nav-link active">
+            Blog
+          </NavLink>
         </li>
         <li className="nav-item">
-          <NavLink to="/contact" className="nav-link active">contact</NavLink>
+          <NavLink to="/contact" className="nav-link active">
+            contact
+          </NavLink>
         </li>
         <li className="nav-item">
-          <NavLink to="/faq" className="nav-link active">FAQ</NavLink>
+          <NavLink to="/faq" className="nav-link active">
+            FAQ
+          </NavLink>
         </li>
       </ul>
     </nav>
